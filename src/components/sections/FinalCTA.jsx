d140ed1c16520ae723dce19a74b71e2c274679fe
AbhinavKRNarayan <abhinavv2005@gmail.com>
2024-11-30 20:13:17 +0530

    changes

--- src/components/sections/FinalCTA.jsx
+++ src/components/sections/FinalCTA.jsx
@@ -4,18 +4,18 @@ import ring from '../../assets/ring.png';
 
 const FinalCTA = () => {
   return (
-    <section className="relative w-full bg-black py-32 md:py-48"> 
+    <section className="relative w-full bg-black py-8 md:py-32"> 
       <div className="container mx-auto">
-        <div className="flex flex-col items-center justify-center">
+        <div className="flex flex-col items-center justify-center pt-8 md:pt-0">
           <img 
             src={circlePhone}
             alt="Phone interface in circle"
-            className="w-[85%] sm:w-[90%] md:w-full max-w-2xl object-contain relative z-10 md:-mr-32" 
+            className="w-[85%] sm:w-[90%] md:w-full max-w-2xl object-contain relative z-10 ml-20 sm:ml-12 md:ml-0 mb-[-5%] sm:mb-[-15%] md:mb-0 md:-mr-32" 
           />
           <img 
             src={ring}
             alt="Decorative ring"
-            className="w-[85%] sm:w-[90%] md:w-full max-w-2xl object-contain -mt-[55%] sm:-mt-[45%] md:-mt-32 md:-ml-32"
+            className="w-[85%] sm:w-[90%] md:w-full max-w-2xl object-contain mt-[-10%] sm:mt-[-20%] md:-mt-32 md:-ml-32"
           />
         </div>
       </div>
@@ -23,4 +23,4 @@ const FinalCTA = () => {
   );
 };
 
-export default FinalCTA;
+export default FinalCTA;  
